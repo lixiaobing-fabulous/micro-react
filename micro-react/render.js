7f51bad2ace89ff5f1bd710c4ae255334cbc6457
lixiaobing.alex <lixiaobing.alex@bytedance.com>
2024-07-30 09:46:49 +0800

    Fibers

--- micro-react/render.js
+++ micro-react/render.js
@@ -1,19 +1,26 @@
-function render(element, container) {
+function createDOM(fiber) {
     // 创建dom对象
-    const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode(element)
-        : document.createElement(element.type);
+    const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('')
+        : document.createElement(fiber.type);
 
     // 赋值属性
-    Object.keys(element.props)
+    Object.keys(fiber.props)
         .filter(key => key !== 'children')
-        .forEach(key => dom[key] = element.props[key]);
+        .forEach(key => dom[key] = fiber.props[key]);
 
-    // 递归，渲染每一个child
-    element.props.children.forEach(child => render(child, dom));
+    return dom;
+}
 
-    // 添加element到父节点
-    container.appendChild(dom);
 
+function render(element, container) {
+    // Root fiber
+    nextUnitOfWork = {
+        dom: container,
+        props: {
+            children: [element],
+        },
+        child: null,
+    };
 }
 
 let nextUnitOfWork = null;
@@ -34,7 +41,55 @@ function workLoop(deadline) {
 // 空闲时渲染
 requestIdleCallback(workLoop);
 
+
 // 执行一个渲染单元任务，并返回新的任务
-function performUnitOfWork(nextUnitOfWork) {}
+function performUnitOfWork(fiber) {
+    if (!fiber.dom) {
+        fiber.dom = createDOM(fiber);
+    }
+
+    // 渲染DOM
+    if (fiber.parent) {
+        fiber.parent.dom.appendChild(fiber.dom);
+    }
+
+    // 给children创建fiber
+    const elements = fiber.props.children;
+    let prevSibling = null;
+    for (let i = 0; i < elements.length; i++) {
+        const newFiber = {
+            type: elements[i].type,
+            props: elements[i].props,
+            parent: fiber,
+            child: null,
+            sibling: null,
+            dom: null,
+        };
+
+        // 第一个child才会被视为child,其他的是算child的sibling
+        if (i === 0) {
+            fiber.child = newFiber;
+        } else {
+            prevSibling.sibling = newFiber;
+        }
+        prevSibling = newFiber;
+    }
+    // 如果有child就返回child
+    if (fiber.child) {
+        return fiber.child;
+    }
+
+    // 如果没有child就返回兄弟，向上查找
+    let nextFiber = fiber;
+    while (nextFiber) {
+        // 有sibling
+        if (nextFiber.sibling) {
+            return nextFiber.sibling;
+        }
+        nextFiber = nextFiber.parent;
+    }
+
+
+}
 
 export default render;
