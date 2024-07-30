03903b38816ef3c49eaddad4a3577c432982c9cf
lixiaobing.alex <lixiaobing.alex@bytedance.com>
2024-07-30 09:24:30 +0800

    Concurrent Mode

--- micro-react/render.js
+++ micro-react/render.js
@@ -16,4 +16,25 @@ function render(element, container) {
 
 }
 
+let nextUnitOfWork = null;
+
+// 调度
+function workLoop(deadline) {
+    // shouldYield 表示线程繁忙，应该中断渲染
+    let shouldYield = false;
+    while (nextUnitOfWork && !shouldYield) {
+        nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+        // shouldYield 检查线程是否繁忙
+        shouldYield = deadline.timeRemaining() < 1;
+    }
+    // 重新请求
+    requestIdleCallback(workLoop);
+}
+
+// 空闲时渲染
+requestIdleCallback(workLoop);
+
+// 执行一个渲染单元任务，并返回新的任务
+function performUnitOfWork(nextUnitOfWork) {}
+
 export default render;
